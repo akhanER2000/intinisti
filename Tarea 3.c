2b61cfb903c040b333a1afa1d4678bbbf5edba19
akhanER2000 <castrolorenzosegundo@gmail.com>
2023-06-20 18:50:13 -0400

    inicial

--- Tarea 3.c
+++ Tarea 3.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 struct Nodo {
     char rut[20];
     char nombre[100];
@@ -9,7 +8,7 @@ struct Nodo {
     struct Nodo* izquierda;
     struct Nodo* derecha;
 };
-
+// Función para crear un nuevo nodo del árbol
 struct Nodo* crearNodo(char rut[], char nombre[], int valor) {
     struct Nodo* nodo = (struct Nodo*)malloc(sizeof(struct Nodo));
     strcpy(nodo->rut, rut);
@@ -19,74 +18,62 @@ struct Nodo* crearNodo(char rut[], char nombre[], int valor) {
     nodo->derecha = NULL;
     return nodo;
 }
-
+// Función para insertar un nodo en el árbol
 struct Nodo* insertar(struct Nodo* raiz, struct Nodo* nodo) {
     if (raiz == NULL) {
         return nodo;
     }
-
     if (strcmp(raiz->rut, nodo->rut) < 0) {
         raiz->derecha = insertar(raiz->derecha, nodo);
-    } else if (strcmp(raiz->rut, nodo->rut) > 0) {
+    } 
+    else if (strcmp(raiz->rut, nodo->rut) > 0) {
         raiz->izquierda = insertar(raiz->izquierda, nodo);
-    } else {
+    } 
+    else {
         raiz->valor += nodo->valor;
         free(nodo);
     }
-
     return raiz;
 }
-
+// Función para buscar un nodo en el árbol por su rut
 struct Nodo* buscarRut(const char rut[], struct Nodo* raiz) {
     if (raiz == NULL || strcmp(raiz->rut, rut) == 0) {
         return raiz;
     }
-
     if (strcmp(raiz->rut, rut) < 0) {
         return buscarRut(rut, raiz->derecha);
     }
-
     return buscarRut(rut, raiz->izquierda);
 }
-
+// Función auxiliar para actualizar los ruts y generar el archivo de salida
 void generarArchivoSDE(const char* archivoEntrada) {
     char archivoSalida[100];
     strcpy(archivoSalida, archivoEntrada);
     int len = strlen(archivoSalida);
     strcpy(archivoSalida + len - 3, "sde");
-
     FILE* archivo = fopen(archivoEntrada, "r");
     FILE* archivoSalidaPtr = fopen(archivoSalida, "w");
-
     if (archivo == NULL || archivoSalidaPtr == NULL) {
         printf("Error al abrir los archivos.\n");
         return;
     }
-
     struct Nodo* arbol = NULL;
     int totalPersonas = 0;
-
     char linea[200];
     while (fgets(linea, sizeof(linea), archivo)) {
         char rut[20], nombre[100];
         int valor;
         sscanf(linea, "%[^,],%[^,],%d", rut, nombre, &valor);
-
         struct Nodo* nodo = crearNodo(rut, nombre, valor);
         arbol = insertar(arbol, nodo);
         totalPersonas++;
     }
-
     int personasMasDeDosEntradas = 0;
     generarArchivoSDERecursivo(arbol, archivoSalidaPtr, &personasMasDeDosEntradas);
-
     fprintf(archivoSalidaPtr, "Total de personas: %d\n", personasMasDeDosEntradas);
-
     fclose(archivo);
     fclose(archivoSalidaPtr);
-
     liberarArbol(arbol);
-
     if (personasMasDeDosEntradas > 0) {
         printf("El archivo %s fue generado.\n", archivoSalida);
     } else {
@@ -94,21 +81,18 @@ void generarArchivoSDE(const char* archivoEntrada) {
         printf("Todos solicitaron la cantidad correcta de entradas.\n");
     }
 }
-
-
+// Función auxiliar recursiva para generar el archivo de salida
 void generarArchivoSDERecursivo(struct Nodo* raiz, FILE* archivoSalida, int* contador) {
     if (raiz) {
         generarArchivoSDERecursivo(raiz->izquierda, archivoSalida, contador);
-
         if (raiz->valor > 2) {
             fprintf(archivoSalida, "%s, %s: %d\n", raiz->rut, raiz->nombre, raiz->valor);
             (*contador)++;
         }
-
         generarArchivoSDERecursivo(raiz->derecha, archivoSalida, contador);
     }
 }
-
+// Función para liberar la memoria del árbol
 void liberarArbol(struct Nodo* raiz) {
     if (raiz) {
         liberarArbol(raiz->izquierda);
@@ -116,13 +100,10 @@ void liberarArbol(struct Nodo* raiz) {
         free(raiz);
     }
 }
-
 int main() {
     char archivoEntrada[100];
     printf("Ingrese el nombre del archivo de entrada: ");
     scanf("%s", archivoEntrada);
-
     generarArchivoSDE(archivoEntrada);
-
     return 0;
 }
